84c801dc8fe589efa82ec9e9904f38f027b8cfa5
chiash5732 <chiash5732@gmail.com>
2020-01-04 17:47:34 +0800

    add the test

--- Sigma.cpp
+++ Sigma.cpp
@@ -1,8 +1,22 @@
-#include<iostream>
+//程式設計期末考試
+//結構判斷式    1.if  2.if   else   3.if     else  if    3.switch()    case
+//迴圈          1.for   2.while   3.do   while
+#include <iostream>
+#include <stdlib.h>
 using namespace std;
-
 int main()
 {
-    
-    return 0;
+    int a,b,c,sum;
+     cout << "請輸入加總開始值?";
+     cin >> a;
+     cout << "請輸入加總終止值?";
+     cin >> b;
+     cout << "請輸入遞增減值?";
+     cin >> c;
+     for(int i=a;i<=b;i+=c){
+     	sum+=i;
+     	cout <<"i="<<i<<",sum="<<sum<<"\n";
+	 }
+	 system ("pause");
+	 return 0;
 }
