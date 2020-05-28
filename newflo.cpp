28eceaab12da6463cfdbf111cc447a5e8d8951b0
chiash5732 <chiash5732@gmail.com>
2020-05-28 19:20:32 +0800

    teak new f

---
+++ newflo.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include <string>
+#include<stdio.h>
+#include<string.h>
+using namespace std;
+
+int main()
+{
+    string saa = "mkdir tree\\d";
+    for (int i = 0; i <= 2; i++)
+    {
+        for (int j = 0; j <= 10; j++){
+            saa = saa + "d\\dd\\d";
+        }
+    }
+    saa = saa + "d tree\\a";
+        for (int i = 0; i <= 2; i++)
+    {
+        for (int j = 0; j <= 10; j++){
+            saa = saa + "a\\aa\\a";
+        }
+    }
+    saa = saa + "a tree\\b";
+        for (int i = 0; i <= 2; i++)
+    {
+        for (int j = 0; j <= 10; j++){
+            saa = saa + "b\\bb\\b";
+        }
+    }
+    saa = saa + "b tree\\c";
+        for (int i = 0; i <= 2; i++)
+    {
+        for (int j = 0; j <= 10; j++){
+            saa = saa + "c\\cc\\c";
+        }
+    }
+    saa = saa + "c";
+    cout << saa.c_str() << endl;
+    system("pause");
+    system(saa.c_str());
+    return 0;
+}
